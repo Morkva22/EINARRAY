3a2f25b95bccd4bdf43597465d84a24c4fb892cb
Mark Maichenko <marcusmay0202@gmail.com>
2024-12-16 19:19:49 +0200

    donme

--- EINARRAY.cpp
+++ EINARRAY.cpp
@@ -2,6 +2,15 @@
 using namespace std;
 int main()
 {
-    
+    const int SIZE = 5;
+    int arr[SIZE];
+    int sum = 0;
+    for (int i = 0; i < SIZE; i++)
+    {
+        cout << "Enter a number: ";
+        cin >> arr[0];
+        sum += arr[0];
+    }
+    cout << "P: " << sum << endl;
     
 }
