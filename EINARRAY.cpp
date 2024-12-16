6554ff1da1edad9ff6c9e65ca2a585948adffe9b
Mark Maichenko <marcusmay0202@gmail.com>
2024-12-16 19:05:12 +0200

    done

--- EINARRAY.cpp
+++ EINARRAY.cpp
@@ -2,6 +2,12 @@
 using namespace std;
 int main()
 {
+    const int SIZE = 10;
+    int arr[SIZE] = {0,1,2,3,4,5,6,7,8,9};
     
-    
+    cout << "negative ";
+    for (int i = SIZE - 1; i >= 0; i--){
+        cout << arr[i];
+    }
+    cout<< endl;
 }
