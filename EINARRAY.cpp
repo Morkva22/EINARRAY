7ea9e3a55cf599131f81ca04667c810cf5a94d36
Mark Maichenko <marcusmay0202@gmail.com>
2024-12-16 18:11:50 +0200

    donet

--- EINARRAY.cpp
+++ EINARRAY.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
+#include <ctime>
 using namespace std;
-int main()
+
+void main()
 {
-    
-}
+    const int SIZE = 10;
+    int arr[SIZE];
+    srand(time(0));
+
+    for (int i = 0; i < SIZE; i++)
+    {
+        arr[i] = rand() % 912;
+        cout<< arr[i] << " ";
+    }
+    cout <<endl;
+
+    int min = arr[0];
+    int max = arr[0];
+
+    for (int i=1; i < SIZE; i++)
+    {
+        if ( arr[i] < min)
+        {
+            min = arr[i];
+        }
+        if ( arr[i] > max)
+        {
+            max = arr[i];
+        }
+    }
+        cout << "Min: " << min << endl;
+        cout << "Max: " << max << endl;
+    }
+
