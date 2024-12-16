aef4bf644fa4ed753493b3fd6d1bb22202435371
Mark Maichenko <marcusmay0202@gmail.com>
2024-12-16 19:09:50 +0200

    done

--- EINARRAY.cpp
+++ EINARRAY.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 using namespace std;
-int main()
-{
-    
-    
+
+int main() {
+    const int MONTHS = 6;
+    int furion [MONTHS];
+
+    cout << "Enter the furion for each month:" << endl;
+    for (int i = 0; i < MONTHS; ++i) {
+        cout << "Month " << i + 1 << ": ";
+        cin >> furion[i];
+    }
+    int sum = 0;
+    for (int i = 0; i < MONTHS; ++i) {
+        sum += furion[i];
+    }
+
+    cout << "furion in month " << sum + 1 << " with a furion of " << sum << endl;
 }
